747148a122a16e300ab971d850b4a85c1e9c71ea
Daylin Morgan <me@dayl.in>
2026-08-18 14:54:36 -0400

    new format of section

--- tasks.py
+++ tasks.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-# fmt: off
-# https://swydd.dayl.in/#automagic-snippet
-if not((_i:=__import__)("importlib.util").util.find_spec("swydd")or
-(_src:=_i("pathlib").Path(__file__).parent/"swydd/__init__.py").is_file()):
-  _r=_i("urllib.request").request.urlopen("https://swydd.dayl.in/swydd.py")
-  _src.parent.mkdir(exist_ok=True);_src.write_text(_r.read().decode())  # noqa
-# fmt: on
+if not (  # https://swydd.dayl.in/#automagic-snippet
+    (_i := __import__)("importlib.util").util.find_spec("swydd")
+    or (_src := _i("pathlib").Path(__file__).parent / "swydd/__init__.py").is_file()
+):
+    _r = _i("urllib.request").request.urlopen("https://swydd.dayl.in/swydd.py")
+    _src.parent.mkdir(exist_ok=True)
+    _src.write_text(_r.read().decode())
 
 from swydd import task, cli, sub
 
